0dbdacb2412aa480be366c0fb682c86c05649101
Nathan Harms <nharms@uwaterloo.ca>
2024-01-05 07:32:09 +0100

    SODA 2024 slides

---
+++ soda2024.html.pm
@@ -0,0 +1,47 @@
+#lang pollen
+
+◊logo{}
+
+◊separator-large
+
+◊art_large["/arts/soda2024/randomized_communication_transparent_large.png" "Randomized Communication"]{
+  ◊bf{SODA 2024 Slides.} Jan. 2024.
+  
+  ◊bf{Pen:} Platinum 3776 Century (UEF nib) for black, various others for colors.
+
+  ◊bf{Ink:} Platinum carbon black, Diamine wild strawberry, Diamine ancient copper, Diamine aurora
+borealis, Iroshizuku ama-iro.
+
+  ◊bf{Paper:} Midori MD, A4.
+
+  My slides for the paper ◊it{Randomized Communication and Implicit Representations for Matrices and
+Graphs of Small Sign-Rank}, coauthored with Viktor Zamaraev and presented at SODA 2024.
+
+  While making the title slide I was regretting not finding a shorter title for the paper...
+
+  ◊link["/arts/soda2024/slides.pdf"]{All slides (PDF).}
+
+  ◊link["/arts/soda2024/title_large.png"]{Randomized communication slide
+    (opaque).}
+
+  ◊link["/arts/soda2024/title_large.png"]{Title slide (opaque).}
+
+}
+
+◊separator-mid
+
+◊panel-gallery{
+◊art[#f "/arts/soda2024/scan_00.png" "Slide 1"]{}
+◊art[#f "/arts/soda2024/scan_01.png" "Slide 2"]{}
+◊art[#f "/arts/soda2024/scan_02.png" "Slide 3"]{}
+◊art[#f "/arts/soda2024/scan_03.png" "Slide 4"]{}
+◊art[#f "/arts/soda2024/scan_04.png" "Slide 5"]{}
+◊art[#f "/arts/soda2024/scan_05.png" "Slide 6"]{}
+◊art[#f "/arts/soda2024/scan_06.png" "Slide 7"]{}
+◊art[#f "/arts/soda2024/scan_07.png" "Slide 8"]{}
+◊art[#f "/arts/soda2024/scan_08.png" "Slide 9"]{}
+◊art[#f "/arts/soda2024/scan_09.png" "Slide 10"]{}
+◊art[#f "/arts/soda2024/scan_10.png" "Slide 11"]{}
+}
+
+◊separator-mid
